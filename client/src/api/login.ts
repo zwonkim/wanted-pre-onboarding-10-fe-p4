6e3de318ceda1cb6d19106bbacecb8437f055f75
zwonkim <k980912@naver.com>
2023-10-13 20:29:45 +0900

    feat: 관리자 권한별 데이터 패칭

--- client/src/api/login.ts
+++ client/src/api/login.ts
@@ -1,49 +1,69 @@
-import { Item, User } from '../types/user'
-import { BASE_URL } from './const'
+import { Item, User } from "../types/user";
+import { BASE_URL } from "./const";
 
-type LoginResult = 'success' | 'fail'
+type LoginResult = "success" | "fail";
 
 export interface LoginRequest {
-  username: string
-  password: string
+  username: string;
+  password: string;
 }
 
 export const login = async (args: LoginRequest): Promise<LoginResult> => {
   const loginRes = await fetch(`${BASE_URL}/auth/login`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      credentials: 'include'
+      "Content-Type": "application/json",
+      "credentials": "include",
     },
-    body: JSON.stringify(args)
-  })
+    body: JSON.stringify(args),
+  });
 
-  return loginRes.ok ? 'success' : 'fail'
-}
+  return loginRes.ok ? "success" : "fail";
+};
 
 export const getCurrentUserInfo = async (): Promise<User | null> => {
-  const userInfoRes = await fetch(`${ BASE_URL }/profile`, {
-    method: 'GET',
+  const userInfoRes = await fetch(`${BASE_URL}/profile`, {
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
-      credentials: 'include'
-    }
-  })
+      "Content-Type": "application/json",
+      "credentials": "include",
+    },
+  });
 
-  return userInfoRes.ok ? userInfoRes.json() : null
-}
+  return userInfoRes.ok ? userInfoRes.json() : null;
+};
 
 // TODO 4-2: GET, '/items' 호출
 export const getItems = async (): Promise<Item[] | null> => {
-  return null
-}
+  const itemsRes = await fetch(`${BASE_URL}/items`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "credentials": "include",
+    },
+  });
+  return itemsRes.ok ? itemsRes.json() : null;
+};
 
 // TODO 4-2: GET, '/all-items' 호출
 export const getAllItems = async (): Promise<Item[] | null> => {
-  return null
-}
-
+  const allItemsRes = await fetch(`${BASE_URL}/all-items`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "credentials": "include",
+    },
+  });
+  return allItemsRes.ok ? allItemsRes.json() : null;
+};
 
 // TODO 4-2: POST, '/logout' 호출
 export const logout = async (): Promise<void> => {
-}
+  await fetch(`${BASE_URL}/logout`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "credentials": "include",
+    },
+  });
+};
